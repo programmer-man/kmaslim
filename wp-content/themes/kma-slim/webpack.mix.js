9d63eaca139bb1a3933effe6148f3e5e28438b10
programmer-man <bryan@kerigan.com>
2018-03-13 22:39:30 -0400

    updates

--- wp-content/themes/kma-slim/webpack.mix.js
+++ wp-content/themes/kma-slim/webpack.mix.js
@@ -1,4 +1,38 @@
 const { mix } = require('laravel-mix');
+const purgeCss = require('purgecss-webpack-plugin');
+const glob = require('glob');
 
 mix.js('js/app.js', './')
     .sass('sass/style.scss', './');
+
+mix.options({
+    postCss: [
+        require('autoprefixer')({
+            grid: true,
+            browsers: ['last 2 versions', 'IE 9', 'Safari 9']
+        })
+    ]
+});
+
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new purgeCss({
+                paths: glob.sync([
+                    path.join(__dirname, 'template-parts/**/*.php'),
+                    path.join(__dirname, 'js/**/*.vue')
+                ]),
+                extractors: [
+                    {
+                        extractor: class {
+                            static extract(content) {
+                                return content.match(/[A-z0-9-:\/]+/g)
+                            }
+                        },
+                        extensions: ['html', 'js', 'php', 'vue']
+                    }
+                ]
+            })
+        ]
+    })
+}
